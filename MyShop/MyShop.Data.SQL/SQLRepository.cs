7f1de3e7e7ba3e3647986fed6a7e9bfad69bff02
Renz <rkbriones@gmail.com>
2021-09-03 19:20:25 +0800

    Implemented SQL Repository

---
+++ MyShop/MyShop.Data.SQL/SQLRepository.cs
@@ -0,0 +1,60 @@
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Data.SQL
+{
+    public class SQLRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        internal AppDbContext _db;
+        internal DbSet<T> dbSet;
+
+        public SQLRepository(AppDbContext db)
+        {
+            _db = db;
+            dbSet = _db.Set<T>();
+        }
+        public IQueryable<T> Collection()
+        {
+            return dbSet;
+        }
+
+        public void Commit()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Delete(string id)
+        {
+            var item = dbSet.Find(id);
+
+            if (_db.Entry(item).State == EntityState.Detached)
+            {
+                dbSet.Attach(item);
+            }
+
+            dbSet.Remove(item);
+        }
+
+        public T Find(string id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public void Insert(T item)
+        {
+            dbSet.Add(item);
+        }
+
+        public void Update(T item)
+        {
+            dbSet.Attach(item);
+            _db.Entry(item).State = EntityState.Modified;
+        }
+    }
+}
--- MyShop/MyShop.WebUI/App_Start/UnityConfig.cs
+++ MyShop/MyShop.WebUI/App_Start/UnityConfig.cs
@@ -1,6 +1,7 @@
 using MyShop.Core.Contracts;
 using MyShop.Core.Models;
 using MyShop.Data.InMem;
+using MyShop.Data.SQL;
 using System;
 
 using Unity;
@@ -45,8 +46,8 @@ namespace MyShop.WebUI
 
             // TODO: Register your type's mappings here.
             // container.RegisterType<IProductRepository, ProductRepository>();
-            container.RegisterType<IRepository<Product>, InMemRepository<Product>>();
-            container.RegisterType<IRepository<ProductCategory>, InMemRepository<ProductCategory>>();
+            container.RegisterType<IRepository<Product>, SQLRepository<Product>>();
+            container.RegisterType<IRepository<ProductCategory>, SQLRepository<ProductCategory>>();
         }
     }
 }
