8c8b29802cc581231a04ed30262213b343f1113a
Renz <rkbriones@gmail.com>
2021-09-11 19:01:29 +0800

    update

--- MyShop/MyShop.WebUI.Tests/Controllers/BasketControllerTest.cs
+++ MyShop/MyShop.WebUI.Tests/Controllers/BasketControllerTest.cs
@@ -5,6 +5,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MyShop.Core.Contracts;
 using MyShop.Core.Models;
 using MyShop.Services;
+using MyShop.WebUI.Controllers;
 using MyShop.WebUI.Tests.Mocks;
 
 namespace MyShop.WebUI.Tests.Controllers
@@ -28,6 +29,28 @@ namespace MyShop.WebUI.Tests.Controllers
             Assert.IsNotNull(basket.BasketItems);
             Assert.AreEqual(1, basket.BasketItems.Count());
             Assert.AreEqual("1", basket.BasketItems.FirstOrDefault().ProductId);
+        }
+
+        [TestMethod]
+        public void CanGetBasketSummaryViewModel()
+        {
+            IRepository<Basket> basketRepo = new MockContext<Basket>();
+            IRepository<Product> prodRepo = new MockContext<Product>();
+            MockHttpContext httpContext = new MockHttpContext();
+
+            prodRepo.Insert(new Product() { Id = "1", Price = 100.00m });
+            prodRepo.Insert(new Product() { Id = "2", Price = 50.00m });
+
+            Basket basket = new Basket();
+            basket.BasketItems.Add(new BasketItem() { ProductId="1", Quantity=2 });
+            basket.BasketItems.Add(new BasketItem() { ProductId="2", Quantity=1 });
+            basketRepo.Insert(basket);
+            
+            BasketService basketService = new BasketService(basketRepo, prodRepo);
+
+            var controller = new BasketController(basketService);
+            httpContext.Request.Cookies.Add(new HttpCookie("eCommerceBasket") { Value = basket.Id });
+            controller.ControllerContext = new System.Web.Mvc.ControllerContext(httpContext, new System.Web.Routing.RouteData(), controller);
         }
     }
 }
